bf10a48eeba11b54105af993714301aea8f7ce68
go2023 <tetyanago2018@gmail.com>
2024-02-27 15:08:48 -0800

    Routing was created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import Home from "./components/pages/home";
+import Favorites from "./components/pages/favorites";
+import Details from "./components/pages/details";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <div className="min-h-screen p-6 bg-white text-gray-600 text-lg">
+          <Navbar />
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/favorites" element={<Favorites />} />
+              <Route path="/recipe-item/:id" element={<Details />} />
+          </Routes>
+      </div>
+
     </div>
   );
 }
