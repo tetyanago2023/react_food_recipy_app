b0d397013dd56b4f0a686ea3fa716674682f6d4a
go2023 <tetyanago2018@gmail.com>
2024-02-27 15:57:55 -0800

    context implementation was started

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,19 +1,23 @@
 import {NavLink} from "react-router-dom";
+import {useContext} from "react";
+import {GlobalContext} from "../../context";
 
 const Navbar = () => {
-    // const { searchParam, setSearchParam , handleSubmit } = useContext(GlobalContext);
+    const { searchParam, setSearchParam, handleSubmit } = useContext(GlobalContext);
+
+    console.log(searchParam);
 
     return (
         <nav className="flex justify-between items-center py-8 container mx-auto flex-col lg:flex-row gap-5 lg:gap-0">
             <h2 className="text-2xl font-semibold">
                 <NavLink to={"/"}>FoodRecipe</NavLink>
             </h2>
-            <form >
+            <form onSubmit={handleSubmit}>
                 <input
                     type="text"
                     name="search"
-                    // value={searchParam}
-                    // onChange={(event) => setSearchParam(event.target.value)}
+                    value={searchParam}
+                    onChange={(event) => setSearchParam(event.target.value)}
                     placeholder="Enter Items..."
                     className="bg-white/75 p-3 px-8 rounded-full outline-none lg:w-96 shadow-lg shadow-red-100 focus:shadow-red-200"
                 />
--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,7 +1,30 @@
+import {createContext, useState} from "react";
+
+export const GlobalContext = createContext(null);
+
 const GlobalState = ({ children }) => {
+    const [searchParam, setSearchParam] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = async (event) => {
+        try{
+            const res = await fetch(
+                `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`
+            );
+
+            const data = await res.json();
+            console.log(data);
+
+
+        }catch(error){
+            console.log(error);
+        }
+    }
 
     return (
-        <div></div>
+        <GlobalContext.Provider value={{searchParam, setSearchParam, handleSubmit}}>
+            {children}
+        </GlobalContext.Provider>
     );
 };
 
