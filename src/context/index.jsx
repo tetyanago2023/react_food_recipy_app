57fddd1b00bcea83a29d99c19da500fa6ff7ceb7
go2023 <tetyanago2018@gmail.com>
2024-03-10 18:39:37 -0700

    copilot was updated

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -5,8 +5,10 @@ export const GlobalContext = createContext(null);
 const GlobalState = ({ children }) => {
     const [searchParam, setSearchParam] = useState("");
     const [loading, setLoading] = useState(false);
+    const [recipeList, setRecipeList] = useState([]);
 
     const handleSubmit = async (event) => {
+        event.preventDefault();
         try{
             const res = await fetch(
                 `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`
@@ -15,14 +17,23 @@ const GlobalState = ({ children }) => {
             const data = await res.json();
             console.log(data);
 
+            if(data?.data?.recipes) {
+                setRecipeList(data?.data?.recipes);
+                setLoading(false);
+                setSearchParam("");
+
+            }
+
 
         }catch(error){
             console.log(error);
+            setLoading(false);
+            setSearchParam("");
         }
     }
-
+    console.log(loading, recipeList);
     return (
-        <GlobalContext.Provider value={{searchParam, setSearchParam, handleSubmit}}>
+        <GlobalContext.Provider value={{searchParam, loading, recipeList, setSearchParam, handleSubmit}}>
             {children}
         </GlobalContext.Provider>
     );
